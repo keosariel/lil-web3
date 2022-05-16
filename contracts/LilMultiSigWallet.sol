b100d479f04d3afac1d20d9c4fb0872a0ffa62e7
kenneth gabriel <31022243+keosariel@users.noreply.github.com>
2022-05-17 04:06:45 +0800

    update: removed irrelevant comment :)

--- contracts/LilMultiSigWallet.sol
+++ contracts/LilMultiSigWallet.sol
@@ -27,7 +27,7 @@ contract LilMultiSigWallet{
 
     Transaction[] public transactions;
     mapping(uint => mapping(address => bool)) public approveBy;
-    // ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"], 1
+
     constructor(address[] memory _owners, uint _minApproval) payable {
         require(_owners.length > 1, "owners required");
         require(_minApproval > 0 && _owners.length >= _minApproval, "invalid min approval");
